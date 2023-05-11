a7bffee4eec7036e588b577cccaadf97d34cd8a3
Cheng <42978810+ChengfengTang@users.noreply.github.com>
2023-05-10 22:29:22 -0700

    still need to optimize merging

--- M1.py
+++ M1.py
@@ -1,7 +1,10 @@
+import math
 import os
 import json
 import shelve
+import heapq
 
+from nltk.util import bigrams, trigrams
 import nltk
 from nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
@@ -44,50 +47,82 @@ def tokenize(content):
     # Create a PorterStemmer object
     stemmer = PorterStemmer()
     # Stem tokens
-    tokens = [stemmer.stem(token) for token in tokens]
+    tokens = [stemmer.stem(token) for token in tokens if token.isalnum()]
     # Return the tokens
     return tokens
 
 
 def getIndex(documents):
-    # Create a default dictionary to store the index
-    index = defaultdict(list)
-    # Initialize counters
+    unigramIndex = defaultdict(list)
+    bigramIndex = defaultdict(list)
+    trigramIndex = defaultdict(list)
+    idToURL = {}  # dictionary to map IDs to URLs for retrieval
     counter = 1
-    doc_counter = 0
-    # Iterate over all the documents
+    docCounter = 1  # document counter will now serve as ID
+    totalDocs = len(documents)  # keeps track of the total number of documents
+
     for url, text in documents.items():
-        # Tokenize the text
+        idToURL[docCounter] = url  # map the url with its id
         tokens = tokenize(text)
-        # Get the frequency distribution of the tokens
-        temp = nltk.FreqDist(tokens)
-        # Add the tokens and their frequencies to the index
-        for token, frequency in temp.items():
-            index[token].append([url, frequency])
+        temp1 = nltk.FreqDist(tokens)
+        temp2 = nltk.FreqDist(map(','.join(), bigrams(tokens)))
+        temp3 = nltk.FreqDist(map(','.join(), trigrams(tokens)))
+        # Indexing unigrams
+        for token, frequency in temp1.items():
+            positions = [i for i, t in enumerate(tokens) if t == token]  # Find positions of the token
+            idf = math.log(totalDocs / len(unigramIndex[token])) if unigramIndex[token] else 0  # Calculate IDF
+            tf_idf = frequency * idf  # Calculate TF-IDF
+            unigramIndex[token].append([docCounter, frequency, positions, tf_idf])
 
-        # Increase the document counter
-        doc_counter += 1
-        # If we have processed 1000 documents, save and clear the index
-        if doc_counter >= 1000:
-            saveIndex("inverted index" + str(counter) + ".json", index)
-            index.clear()
-            doc_counter = 0
+        # Indexing bigrams
+        for token, frequency in temp2.items():
+            positions = [i for i, t in enumerate(bigrams(tokens)) if
+                         ' '.join(t) == token]  # Find positions of the token
+            idf = math.log(totalDocs / len(bigramIndex[token])) if bigramIndex[token] else 0  # Calculate IDF
+            tf_idf = frequency * idf  # Calculate TF-IDF
+            bigramIndex[token].append([docCounter, frequency, positions, tf_idf])
+
+        # Indexing trigrams
+        for token, frequency in temp3.items():
+            positions = [i for i, t in enumerate(trigrams(tokens)) if
+                         ' '.join(t) == token]  # Find positions of the token
+            idf = math.log(totalDocs / len(trigramIndex[token])) if trigramIndex[token] else 0  # Calculate IDF
+            tf_idf = frequency * idf  # Calculate TF-IDF
+            trigramIndex[token].append([docCounter, frequency, positions, tf_idf])
+
+        docCounter += 1
+        if docCounter % (len(documents) // 3) == 0:  # Now we save every 1/3
+            saveIndex("unigram index" + str(counter) + ".json", unigramIndex)
+            saveIndex("bigram index" + str(counter) + ".json", bigramIndex)
+            saveIndex("trigram index" + str(counter) + ".json", trigramIndex)
+            unigramIndex.clear()
+            bigramIndex.clear()
+            trigramIndex.clear()
             counter += 1
+        print("ok ", counter, len(unigramIndex), len(bigramIndex), len(trigramIndex))
 
-        # Print the current state
-        print("ok ", counter, len(index))
+    if unigramIndex:  # Save the last partial index if it exists
+        saveIndex("unigram index" + str(counter) + ".json", unigramIndex)
+    if bigramIndex:
+        saveIndex("bigram index" + str(counter) + ".json", bigramIndex)
+    if trigramIndex:
+        saveIndex("trigram index" + str(counter) + ".json", trigramIndex)
 
-    # Save the last partial index if it exists
-    if index:
-        saveIndex("inverted index" + str(counter) + ".json", index)
+    # Also save the ID-URL mappings
+    with open('id to URL.json', 'w') as f:
+        json.dump(idToURL, f)
 
-    # Merge all the indices
-    mergeIndex(counter)
+    # Merge each type of index separately
+    mergeIndex(counter, 'unigram')
+    mergeIndex(counter, 'bigram')
+    mergeIndex(counter, 'trigram')
 
 
-def mergeIndex(counter):
+
+def mergeIndex(counter, path):
+    print("Merging " + path)
     # Use shelve to store the final index on disk
-    with shelve.open('final index') as finalIndex:
+    with shelve.open('final ' + path) as finalIndex:
         # Iterate over all the indices
         for i in range(1, counter + 1):
             # Open the index file
@@ -106,7 +141,7 @@ def saveIndex(path, index):
     # Open a file at the given path
     with open(path, 'w') as f:
         # Dump the index into the file as JSON
-        json.dump(index, f)
+        json.dump(dict(sorted(index.items())), f)
 
 
 if __name__ == "__main__":
