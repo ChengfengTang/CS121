c1b194c57ae8c4756310027edaa0c897e5b429ad
chengfengtang <chengfengtang0903@gmail.com>
2023-05-10 23:07:47 -0700

    Still need to optimize merging

--- M1.py
+++ M1.py
@@ -135,7 +135,7 @@ def mergeIndex(counter, path):
                         finalIndex[token] = posting
                     else:
                         finalIndex[token].extend(posting)
-    print(path, "# of unique tokens ", len(finalIndex))
+        print(path, "# of unique tokens ", len(finalIndex))
     
 
 
